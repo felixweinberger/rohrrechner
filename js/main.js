dcafdc3c409bd91c6a3b6ff1dd56ec65ea6be94b
Felix <felix.weinberger@gmail.com>
2016-04-02 22:21:59 +0200

    Version 0.1, next step organising code

---
+++ js/main.js
@@ -0,0 +1,60 @@
+var app = angular.module('rohrrechner', []);
+
+app.directive('smartFloat', function ($filter) {
+    var FLOAT_REGEXP_1 = /^\$?\d+.(\d{3})*(\,\d*)$/; //Numbers like: 1.123,56
+    var FLOAT_REGEXP_2 = /^\$?\d+,(\d{3})*(\.\d*)$/; //Numbers like: 1,123.56
+    var FLOAT_REGEXP_3 = /^\$?\d+(\.\d*)?$/; //Numbers like: 1123.56
+    var FLOAT_REGEXP_4 = /^\$?\d+(\,\d*)?$/; //Numbers like: 1123,56
+
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function (viewValue) {
+                if (FLOAT_REGEXP_1.test(viewValue)) {
+                    ctrl.$setValidity('float', true);
+                    return parseFloat(viewValue.replace('.', '').replace(',', '.'));
+                } else if (FLOAT_REGEXP_2.test(viewValue)) {
+                        ctrl.$setValidity('float', true);
+                        return parseFloat(viewValue.replace(',', ''));
+                } else if (FLOAT_REGEXP_3.test(viewValue)) {
+                        ctrl.$setValidity('float', true);
+                        return parseFloat(viewValue);
+                } else if (FLOAT_REGEXP_4.test(viewValue)) {
+                        ctrl.$setValidity('float', true);
+                        return parseFloat(viewValue.replace(',', '.'));
+                }else {
+                    ctrl.$setValidity('float', false);
+                    return undefined;
+                }
+            });
+
+            ctrl.$formatters.unshift(
+               function (modelValue) {
+                   return $filter('number')(parseFloat(modelValue) , 3);
+               }
+           );
+        }
+    };
+});
+
+app.controller('rohrrechnerCtrl', function($scope) {
+    // initialise vars for calculations
+    $scope.diameter = "";
+    $scope.thickness = "";
+    $scope.length = "1.000";
+    $scope.number = "1";
+    
+    // initialise outputs
+    $scope.weight = "";
+    $scope.totalLength = "";
+    $scope.totalWeight = "";
+
+    // calculate new values on change to any of the parameters 
+    $scope.$watchGroup(['diameter', 'thickness', 'length', 'number'], function(newValues, oldValues, scope) {
+		  if ($scope.diameter != "" && $scope.thickness != "") {
+			  $scope.weight = ($scope.diameter - $scope.thickness) * $scope.thickness * 0.0246615;
+			  $scope.totalLength = $scope.length * $scope.number;
+			  $scope.totalWeight = $scope.weight * $scope.length * $scope.number;
+		  }
+		});
+});
